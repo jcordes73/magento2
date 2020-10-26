b12a06a4a774ba01caac74ce17a7ab9439ba6f86
Jochen Cordes <jcordes@redhat.com>
2020-10-26 21:39:03 +0100

    Added post-commit script.

---
+++ bin/magento-openshift-post-commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+php -dmemory_limit=1G bin/magento setup:upgrade
+php -dmemory_limit=1G bin/magento setup:di:compile
+php bin/magento deploy:mode:set developer --skip-compilation
+php bin/magento setup:static-content:deploy -f
+php bin/magento cache:clean
+php bin/magento cache:flush

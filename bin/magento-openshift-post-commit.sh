df630893a130b4fdc34362669e7384c14cfcbfcf
Jochen Cordes <jcordes@redhat.com>
2020-10-26 21:59:50 +0100

    Added missing composer install to post commit.

--- bin/magento-openshift-post-commit.sh
+++ bin/magento-openshift-post-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-php composer.phar install
+php composer.phar install $INSTALLER_ARGS
 php -dmemory_limit=1G bin/magento setup:upgrade
 php -dmemory_limit=1G bin/magento setup:di:compile
 php bin/magento deploy:mode:set developer --skip-compilation

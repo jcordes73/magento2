7992fa6da00d254fa967ac1e9efc42e117c36e06
Jochen Cordes <jcordes@redhat.com>
2020-10-27 12:16:44 +0100

    Added install and admin creation to build-cycle.

--- bin/magento-openshift-post-commit.sh
+++ bin/magento-openshift-post-commit.sh
@@ -8,3 +8,5 @@ php bin/magento deploy:mode:set developer --skip-compilation
 php bin/magento setup:static-content:deploy -f
 php bin/magento cache:clean
 php bin/magento cache:flush
+php bin/magento setup:install --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1 --use-secure=1
+php bin/magento admin:user:create --admin-user=$MAGENTO_ADMIN_USER --admin-password=$MAGENTO_ADMIN_PASSWORD --admin-email="$MAGENTO_ADMIN_EMAIL" --admin-firstname="$MAGENTO_ADMIN_FIRSTNAME" --admin-lastname="$MAGENTO_ADMIN_LASTNAME"
